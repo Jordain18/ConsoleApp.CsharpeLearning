4cb2aae85508b5f3e0afbeb81a8f567eb3459ebe
JordainCampbell <60863792+Jordain18@users.noreply.github.com>
2024-08-22 22:51:53 -0500

    Add first and Last Name

--- UserInput/Program.cs
+++ UserInput/Program.cs
@@ -1,11 +1,16 @@
 ﻿// Declare Variables
-string myName = string.Empty;
+using System.Xml.Linq;
+
+string myFirstName = string.Empty;
+string myLastName = string.Empty;
 int myAge =0;
 int retirementAge = 65;
 
 //Prompt  user for input 
-Console.WriteLine("Please  insert your name ");
-myName = Console.ReadLine();
+Console.WriteLine("Please  insert your first name ");
+myFirstName = Console.ReadLine();
+Console.WriteLine("Please  insert your last name ");
+myLastName = Console.ReadLine();
 Console.WriteLine("Please  insert your age ");
 myAge = Convert.ToInt32 (Console.ReadLine());
 
@@ -13,6 +18,6 @@ myAge = Convert.ToInt32 (Console.ReadLine());
 int workingYearsRemaining = retirementAge - myAge;
 
 //Output the results to the user
-Console.WriteLine($"Full name is :{myName}");
+Console.WriteLine($"Full name is :{myFirstName +" "+ myLastName}");
 Console.WriteLine($"Age : {myAge}");
 Console.WriteLine($"Working years Remaining  : {workingYearsRemaining}");
